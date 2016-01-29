88caec202db75f5028248b6125ea7ac372cadb42
Merryfurr <merryfurr@merryfurr.ml>
2016-01-29 15:29:30 -0500

    Custom error code

--- Building Area/Builds/fang.app/Contents/Resources/__error__.sh
+++ Building Area/Builds/fang.app/Contents/Resources/__error__.sh
@@ -1,19 +1,13 @@
-#!/bin/sh
-#
-# This is the default apptemplate error script
-#
 if ( test -n "$2" ) ; then 
 	echo "$1 Error"
-	echo "An unexpected error has occurred during execution of the main script"
+	echo "An unexpected error has occurred during execution of FANG!"
+	echo "This can be caused by the script you are loading or FANG itself!"
 	echo ""
 	echo "$2: $3"
 	echo ""
-	echo "See the Console for a detailed traceback."
+	echo "See the Console for a detailed traceback of everything that happened."
 else
 	echo "$1 Error"
 
-	# Usage: ERRORURL <anURL> <a button label>, this is used by the 
-	# bundle runner to put up a dialog.
-	#echo "ERRORURL: http://www.python.org/ Visit the Python Website
-# echo "ERRORURL: http://homepages.cwi.nl/~jack/macpython/index.html Visit the MacPython Website"
+echo "ERRORURL: https://github.com/Merryfurr/FANG/issues Start an issue if it is cause by FANG"
 fi
